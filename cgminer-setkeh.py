f8c86ebfb229b190821f968f7879462501e91a6d
chpoqxie <91347473+chpoqxie@users.noreply.github.com>
2022-04-08 04:06:16 +0300

    Update cgminer-setkeh.py
    add fixes for response

--- cgminer-setkeh.py
+++ cgminer-setkeh.py
@@ -18,20 +18,20 @@ import json
 import sys
 
 def linesplit(socket):
-        buffer = socket.recv(4096)
-        done = False
-        while not done:
-                more = socket.recv(4096)
-                if not more:
-                        done = True
-                else:
-                        buffer = buffer+more
-        if buffer:
-                return buffer
+	buffer = socket.recv(4096)
+	done = False
+	while not done:
+		more = socket.recv(4096)
+		if not more:
+			done = True
+		else:
+			buffer = buffer+more
+	if buffer:
+		return buffer
 
 # if len(sys.argv) < 3:
-#       api_ip = '127.0.0.1'
-#       api_port = 4028
+#	api_ip = '127.0.0.1'
+#	api_port = 4028
 #else:
 
 api_ip = sys.argv[1]
@@ -42,12 +42,16 @@ api_command = sys.argv[3].split('|')
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((api_ip,int(api_port)))
 if len(api_command) == 2:
-        s.send(json.dumps({"command":api_command[0],"parameter":api_command[1]}).encode())
+	s.send(json.dumps({"command":api_command[0],"parameter":api_command[1]}).encode())
 else:
-        s.send(json.dumps({"command":api_command[0]}).encode())
+	s.send(json.dumps({"command":api_command[0]}).encode())
+
+#s.send(json.dumps({"command":api_command[0]}).encode())
 
 response = linesplit(s).decode()
 response = response.replace('\x00','')
+response = response.replace(",}","}")
+response = response.replace("\n","")
 response = json.loads(response)
 print(response)
 s.close()
